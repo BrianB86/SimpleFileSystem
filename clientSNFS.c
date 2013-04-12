c54c20d4b6fe6c155ad9d80271c82f9c93938679
BrianB86 <bbattell86@gmail.com>
2013-04-12 17:31:56 -0400

    Added addrinfo structs and created sockets.

--- clientSNFS.c
+++ clientSNFS.c
@@ -10,9 +10,12 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <netdb.h>
 #include <string.h>
-#include <ctype.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 
 /* Reference to the sockaddr struct
  *
@@ -39,14 +42,60 @@
  *
  * uint32_t s_addr; 32-bit int (4 bytes)
  * };
+ *
+ * Reference to the addrinfo struct
+ *
+ * struct addrinfo{
+ *
+ * int	ai_flags	AI_PASSIVE 
+ * int 	ai_family	AF_INET
+ * int 	ai_socktype	SOCK_STREAM
+ * int	ai_protocol	0 for any
+ * size_t	ai_addrlen	size of ai_addr in bytes
+ * struct sockaddr	*ai_addr	
+ * char	*ai_canonname	full cononical hostname
+ *
+ * struct addrinfo	*ai_next	linked list next node
+ * };
+ *
  */
 
 
 
 
 
-static int connect_to_server(const char* ipaddr, char* path, int port)
+static int connect_to_server(const char* ipaddr, char* path, char* port)
 {
+	int setup, sock;
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+
+	memset(&hints, 0, sizeof(hints)); //Makes sure no info is in struct.
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = 0;
+	
+	setup = getaddrinfo(ipaddr,port,&hints,&server_info); //server_info points to a linked list of struct addrinfos each contains sockaddr.
+
+	if(setup != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(setup));
+		return 1;
+	}
+
+	sock = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+	if(sock == -1)
+	{
+		perror("Socket Error");
+		return -1;
+	}
+
+
+
+
 
 }
 
@@ -58,13 +107,12 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if(isdigit(argv[2]) == 0)
-	{
-		fprintf(stderr, "ports only have numbers in them.\n");
-		return 1;
-	}
+connect_to_server(argv[0],argv[1],argv[2]);
 
-	
+//check fuse wether we can make our own connect or connect through fuse.
+
+
+
 	
 
 }
